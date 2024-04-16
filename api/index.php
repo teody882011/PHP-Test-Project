7d7da46dec96b5596f5607c93100315540ac475e
teody882011 <104433681+teody882011@users.noreply.github.com>
2024-04-16 12:55:18 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
-</head>
-<body>
-        <h1>Hello, World!</h1>
-</body>
-</html>
+<?php declare(strict_types = 1);
+
+phpinfo();
